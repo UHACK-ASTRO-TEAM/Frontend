8ce938285e3aa282926608c9503959ae49bbe494
chux <chuxoguh@gmail.com>
2022-10-13 13:25:14 +0100

    header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Admission from "./pages/Admission";
+import Community from "./pages/Community";
+import Explore from "./pages/Explore";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+
+      <Routes>
+        <Route path="admissions-support" element={<Admission />} />
+        <Route path="community" element={<Community />} />
+        <Route path="explore" element={<Explore />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Logo from "../assets/Logo1.png";
+
+const Header = () => {
+  const navigation = [
+    {
+      label: "Explore",
+      path: "/explore",
+    },
+    {
+      label: "Admissions support",
+      path: "/admissions-support",
+    },
+    {
+      label: "Community",
+      path: "/community",
+    },
+  ];
+  return (
+    <header className="flex items-center justify-between mx-[100px] my-[60px]">
+      <div className="cursor-pointer">
+        <Link to={"/"}>
+          <img src={Logo} alt="Logo" />
+        </Link>
+      </div>
+
+      <div>
+        <ul className="flex items-center justify-between gap-5 ">
+          {navigation.map((item, i) => (
+            <Link key={i} to={item.path}>
+              <li
+                className={`cursor-pointer hover:border-b-2 border-b-[#FB7C46]`}
+              >
+                {item.label}
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <button className="bg-[#FB7C460A] py-3 px-4 rounded-lg">Login</button>
+        <button className="ml-4 bg-[#FB7C46] py-3 px-4 rounded-lg ">
+          Sign Up
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
